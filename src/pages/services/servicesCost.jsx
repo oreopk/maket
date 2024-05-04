5163979d7d8963bbbcb02d435b7417e1be4e09e5
PAVEL KOZIN <sart6zaV@yandex.ru>
2024-05-05 00:35:21 +0300

    dfgvdfvd

--- src/pages/services/servicesCost.jsx
+++ src/pages/services/servicesCost.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 import servicepng from '../../assets/service.png';
 import '../services/servicesCost.css';
 import { servicescost } from '../../data/services-cost';
 import ServiceCost from '../../components/servicecost-item/service-cost';
 const ServicesCost = () => {
+  const [priceList, setPriceList] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:8000/services/get-list')
+      .then((res) => res.json())
+      .then((data) => setPriceList(data));
+  }, []);
+  useEffect(() => {
+    console.log(priceList);
+  }, [priceList]);
   function chunkArray(array, chunkSize) {
     const chunks = [];
     for (let i = 0; i < array.length; i += chunkSize) {
@@ -12,7 +22,7 @@ const ServicesCost = () => {
     return chunks;
   }
 
-  const result = chunkArray(servicescost, 4);
+  const result = chunkArray(priceList, 4);
   let x = result.length - 1;
 
   const servicesJSX = [];
